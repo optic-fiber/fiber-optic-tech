41d9c61127e6c040104e5a480878c3bad6af6445
cheroliv <cheroliv@laposte.net>
2019-10-14 05:58:02 +0200

    ____

--- src/test/groovy/com/cheroliv/opticfiber/recap/service/RecapServiceImpUnitTest.groovy
+++ src/test/groovy/com/cheroliv/opticfiber/recap/service/RecapServiceImpUnitTest.groovy
@@ -1,67 +1,60 @@
 package com.cheroliv.opticfiber.recap.service
 
+import com.cheroliv.opticfiber.TestData
+import com.cheroliv.opticfiber.inter.repository.InterRepository
+import com.cheroliv.opticfiber.inter.service.InterDataService
+import groovy.transform.TypeChecked
+import groovy.util.logging.Slf4j
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
 
-//import groovy.transform.TypeChecked
-//import groovy.util.logging.Slf4j
-//import org.junit.jupiter.api.*
-//import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
-//import org.junit.jupiter.api.extension.ExtendWith
-//import org.mockito.InjectMocks
-//import org.mockito.MockitoAnnotations
-//import org.mockito.junit.jupiter.MockitoExtension
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-//import org.springframework.boot.test.mock.mockito.MockBean
+import static org.mockito.BDDMockito.given
+import static org.mockito.MockitoAnnotations.initMocks
 
-//import static org.mockito.BDDMockito.given
-
-//@Slf4j
-//@TypeChecked
-//@WebMvcTest(RecapService)
-//@ExtendWith(MockitoExtension)
-//@TestMethodOrder(OrderAnnotation)
-//@DisplayName('RecapServiceImpUnitTest')
+@Slf4j
+@TypeChecked
+@ExtendWith(MockitoExtension)
+@TestMethodOrder(OrderAnnotation)
+@DisplayName('RecapServiceImpUnitTest')
 class RecapServiceImpUnitTest {
-//    @BeforeAll
-//    static void init() {
-//        MockitoAnnotations.initMocks(this)
-//    }
-//    TestData data = TestData.instance
-//
-//    @InjectMocks
-//    RecapService recapService
-//    @MockBean
-//    InterDataService dataService//= Mockito.mock(InterDataService)
-//
-//
-//    @BeforeEach
-//    void setUp() {
-//        recapService = new RecapServiceImp(dataService)
-//    }
-//
-//    @Test
-//    @Order(1)
-//    @DisplayName('testNomFeuilles_database_is_empty')
-//    void testNomFeuilles_database_is_empty() {
-//        given(dataService.findAllMoisFormatFrParAnnee())
-//                .willReturn([])
-//        assert [] == recapService.nomFeuilles()
-//    }
-//
-//    @Test
-//    @Order(2)
-//    @DisplayName('testNomFeuilles')
-//    void testNomFeuilles() {
-//        given(dataService.findAllMoisFormatFrParAnnee())
-//                .willReturn([['octobre': 2018],
-//                             ['décembre': 2018],
-//                             ['janvier': 2019]] as List<Map>)
-//        assert recapService.nomFeuilles()
-//                .eachWithIndex { String it, int idx ->
-//                    it == ['octobre 2018',
-//                           'décembre 2018',
-//                           'janvier 2019'].get(idx)
-//                }
-//    }
+    @BeforeAll
+    static void init() { initMocks(this) }
+    TestData data = TestData.instance
+
+    @InjectMocks
+    RecapServiceImp recapService
+    @Mock
+    InterDataService dataService
+    @Mock
+    InterRepository repo
 
 
+    @Test
+    @Order(1)
+    @DisplayName('testNomFeuilles_database_is_empty')
+    void testNomFeuilles_database_is_empty() {
+        given(dataService.findAllMoisFormatFrParAnnee())
+                .willReturn([])
+        assert [] == recapService.nomFeuilles()
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName('testNomFeuilles')
+    void testNomFeuilles() {
+        given(dataService.findAllMoisFormatFrParAnnee())
+                .willReturn([['octobre': 2018],
+                             ['décembre': 2018],
+                             ['janvier': 2019]] as List<Map>)
+        assert recapService.nomFeuilles()
+                .eachWithIndex { String it, int idx ->
+                    it == ['octobre 2018',
+                           'décembre 2018',
+                           'janvier 2019'].get(idx)
+                }
+    }
 }
